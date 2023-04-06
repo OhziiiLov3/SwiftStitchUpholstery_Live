1e051f6bc5bb3c51da5afb505bce2bb1af091bd3
ohziiilov3 <klbaskerville0520@gmail.com>
2023-04-06 15:22:28 -0700

    updating blogs.js

--- frontend/src/blog.js
+++ frontend/src/blog.js
@@ -7,7 +7,7 @@ const blog = [
   {
     _id: "1",
     name: "Work Hard, Play Harder...",
-    image: "./images/auto_car2.jpg",
+    image: "/images/auto_car2.jpg",
     date: "November 10th",
     description:
       "Nappa Italia is an OEM style, European leather embossed with the quintessential nappa haircell print. ",
@@ -15,7 +15,7 @@ const blog = [
   {
     _id: "2",
     name: "Nothing like the OG!",
-    image: "./images/Volkswagen_mk1.jpeg",
+    image: "/images/Volkswagen_mk1.jpeg",
     date: "December 10th",
     description:
       "Nappa Italia is an OEM style, European leather embossed with the quintessential nappa haircell print. ",
@@ -23,7 +23,7 @@ const blog = [
   {
     _id: "3",
     name: "How we Started!",
-    image: "./images/PrimaryLightCombo.png",
+    image: "/images/PrimaryLightCombo.png",
     date: "Janurary 20th",
     description:
       "Nappa Italia is an OEM style, European leather embossed with the quintessential nappa haircell print. ",
@@ -31,7 +31,7 @@ const blog = [
   {
     _id: "4",
     name: " My Dream is my Job!",
-    image: "./images/auto_car2.jpg",
+    image: "/images/auto_car2.jpg",
     date: "March 20th",
     description:
       "Nappa Italia is an OEM style, European leather embossed with the quintessential nappa haircell print. ",
