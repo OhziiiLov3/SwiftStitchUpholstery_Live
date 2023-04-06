9b48b80e611ee007f570a82da08af33b2bbe9dd0
ohziiilov3 <klbaskerville0520@gmail.com>
2023-04-06 15:07:31 -0700

    fix blog

--- frontend/src/blog.js
+++ frontend/src/blog.js
@@ -1,13 +1,13 @@
-import autoCart1 from "./images/auto_car1.png";
-import autoCart2 from "./images/Volkswagen_mk1.jpeg";
-import autoCart3 from "./images/PrimaryLightCombo.png";
-import autoCart4 from "./images/auto_car2.jpg";
+// import autoCart1 from "./images/auto_car1.png";
+// import autoCart2 from "./images/Volkswagen_mk1.jpeg";
+// import autoCart3 from "./images/PrimaryLightCombo.png";
+// import autoCart4 from "./images/auto_car2.jpg";
 
 const blog = [
   {
     _id: "1",
     name: "Work Hard, Play Harder...",
-    image: autoCart1,
+    image: "./images/auto_car2.jpg",
     date: "November 10th",
     description:
       "Nappa Italia is an OEM style, European leather embossed with the quintessential nappa haircell print. ",
@@ -15,7 +15,7 @@ const blog = [
   {
     _id: "2",
     name: "Nothing like the OG!",
-    image: autoCart2,
+    image: "./images/Volkswagen_mk1.jpeg",
     date: "December 10th",
     description:
       "Nappa Italia is an OEM style, European leather embossed with the quintessential nappa haircell print. ",
@@ -23,7 +23,7 @@ const blog = [
   {
     _id: "3",
     name: "How we Started!",
-    image: autoCart3,
+    image: "./images/PrimaryLightCombo.png",
     date: "Janurary 20th",
     description:
       "Nappa Italia is an OEM style, European leather embossed with the quintessential nappa haircell print. ",
@@ -31,7 +31,7 @@ const blog = [
   {
     _id: "4",
     name: " My Dream is my Job!",
-    image: autoCart4,
+    image: "./images/auto_car2.jpg",
     date: "March 20th",
     description:
       "Nappa Italia is an OEM style, European leather embossed with the quintessential nappa haircell print. ",
