c76c44bfbc44138100a6681af601c6068d90ddba
ohziiilov3 <klbaskerville0520@gmail.com>
2024-06-06 19:55:03 -0700

    change db name for deployment

--- backend/settings.py
+++ backend/settings.py
@@ -144,8 +144,10 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Feel free to alter this value to suit your needs.
-        default='postgresql://postgres:postgres@localhost:5432/swiftstitchdb',
+        default='postgresql://postgres:postgres@localhost:5432/swfitstitchdb_4gvs',
     )
+
+
 }
         # conn_max_age=1800
 # DATABASE_URL = "postgresql://postgres:Q3oIG9EcTYDjrXTYskvt@containers-us-west-132.railway.app:5513/railway"
