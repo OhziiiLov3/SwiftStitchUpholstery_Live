b82560f6d25d1adc900e4750e2f7a3e2f27cef6a
ohziiilov3 <klbaskerville0520@gmail.com>
2024-06-06 22:22:40 -0700

    fix media root url

--- swiftStitch/settings.py
+++ swiftStitch/settings.py
@@ -193,14 +193,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/images/'
+# MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
     os.path.join(BASE_DIR / 'frontend/build/static')
 ]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-MEDIA_ROOT = BASE_DIR / 'static/images'
+# MEDIA_ROOT = BASE_DIR / 'static/images'
 STATIC_ROOT = os.path.join(BASE_DIR / 'staticfiles')
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
